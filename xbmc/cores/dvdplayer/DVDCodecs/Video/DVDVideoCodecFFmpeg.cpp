052117ad4e508bd2f647d93da60585eb6c3b2236
xbmc <fernetmenta@online.de>
2012-04-12 12:09:31 +0200

    xvba: add decoder

--- xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecFFmpeg.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecFFmpeg.cpp
@@ -57,6 +57,9 @@
 #ifdef HAVE_LIBVA
 #include "VAAPI.h"
 #endif
+#ifdef HAVE_LIBXVBA
+#include "XVBA.h"
+#endif
 
 using namespace boost;
 
@@ -97,6 +100,19 @@ enum PixelFormat CDVDVideoCodecFFmpeg::GetFormat( struct AVCodecContext * avctx
     else
       dec->Release();
   }
+#endif
+#ifdef HAVE_LIBXVBA
+    if(*cur == AV_PIX_FMT_XVBA_VLD && CSettings::Get().GetBool("videoplayer.usexvba"))
+    {
+      XVBA::CDecoder* dec = new XVBA::CDecoder();
+      if(dec->Open(avctx, *cur, ctx->m_uSurfacesCount))
+      {
+        ctx->SetHardware(dec);
+        return *cur;
+      }
+      else
+        dec->Release();
+    }
 #endif
 #ifdef HAVE_LIBVA
     // mpeg4 vaapi decoding is disabled
--- xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
+++ xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
@@ -1189,6 +1189,10 @@ int CDVDPlayerVideo::OutputPicture(const DVDVideoPicture* src, double pts)
         formatstr = "NONE";
         buffering = false;
         break;
+      case RENDER_FMT_XVBA:
+        formatstr = "XVBA";
+        buffering = true;
+        break;
     }
 
     if(m_bAllowFullscreen)
