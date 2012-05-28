17cecb4dc6a3d0837001272b693a806b4834206b
xbmc <fernetmenta@online.de>
2012-05-28 10:17:33 +0200

    drop frame counter in application, ask render manager instead

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -420,8 +420,6 @@ CApplication::CApplication(void)
 #endif
   m_currentStack = new CFileItemList;
 
-  m_frameCount = 0;
-
   m_bPresentFrame = false;
   m_bPlatformDirectories = true;
 
@@ -2048,28 +2046,18 @@ float CApplication::GetDimScreenSaverLevel() const
 
 bool CApplication::WaitFrame(unsigned int timeout)
 {
-  bool done = false;
-
   // Wait for all other frames to be presented
-  CSingleLock lock(m_frameMutex);
-  //wait until event is set, but modify remaining time
+  m_frameEvent.Reset();
 
-  TightConditionVariable<InversePredicate<int&> > cv(m_frameCond, InversePredicate<int&>(m_frameCount));
-  cv.wait(lock,timeout);
-  done = m_frameCount == 0;
+  if (!g_renderManager.HasFrame() && !m_frameEvent.WaitMSec(timeout))
+    return false;
 
-  return done;
+  return g_renderManager.HasFrame();
 }
 
 void CApplication::NewFrame()
 {
-  // We just posted another frame. Keep track and notify.
-  {
-    CSingleLock lock(m_frameMutex);
-    m_frameCount++;
-  }
-
-  m_frameCond.notifyAll();
+  m_frameEvent.Set();
 }
 
 void CApplication::Render()
@@ -2082,7 +2070,6 @@ void CApplication::Render()
 
   int vsync_mode = CSettings::Get().GetInt("videoscreen.vsync");
 
-  bool decrement = false;
   bool hasRendered = false;
   bool limitFrames = false;
   unsigned int singleFrameTime = 10; // default limit 100 fps
@@ -2096,13 +2083,10 @@ void CApplication::Render()
     m_bPresentFrame = false;
     if (!extPlayerActive && g_graphicsContext.IsFullScreenVideo() && !IsPaused() && g_renderManager.RendererHandlesPresent())
     {
-      CSingleLock lock(m_frameMutex);
-
-      TightConditionVariable<int&> cv(m_frameCond,m_frameCount);
-      cv.wait(lock,100);
-
-      m_bPresentFrame = m_frameCount > 0;
-      decrement = m_bPresentFrame;
+      m_frameEvent.Reset();
+      m_bPresentFrame = g_renderManager.HasFrame();
+      if (!m_bPresentFrame && m_frameEvent.WaitMSec(100))
+        m_bPresentFrame = g_renderManager.HasFrame();
       hasRendered = true;
     }
     else
@@ -2126,8 +2110,6 @@ void CApplication::Render()
         else if (lowfps)
           singleFrameTime = 200;  // 5 fps, <=200 ms latency to wake up
       }
-
-      decrement = true;
     }
   }
 
@@ -2192,13 +2174,6 @@ void CApplication::Render()
 
   g_renderManager.UpdateResolution();
   g_renderManager.ManageCaptures();
-
-  {
-    CSingleLock lock(m_frameMutex);
-    if(m_frameCount > 0 && decrement)
-      m_frameCount--;
-  }
-  m_frameCond.notifyAll();
 }
 
 void CApplication::SetStandAlone(bool value)
@@ -5517,12 +5492,6 @@ bool CApplication::SwitchToFullScreen()
   // See if we're playing a video, and are in GUI mode
   if ( IsPlayingVideo() && g_windowManager.GetActiveWindow() != WINDOW_FULLSCREEN_VIDEO)
   {
-    // Reset frame count so that timing is FPS will be correct.
-    {
-      CSingleLock lock(m_frameMutex);
-      m_frameCount = 0;
-    }
-
     // then switch to fullscreen mode
     g_windowManager.ActivateWindow(WINDOW_FULLSCREEN_VIDEO);
     return true;
@@ -5755,7 +5724,6 @@ bool CApplication::IsCurrentThread() const
 
 bool CApplication::IsPresentFrame()
 {
-  CSingleLock lock(m_frameMutex);
   bool ret = m_bPresentFrame;
 
   return ret;
