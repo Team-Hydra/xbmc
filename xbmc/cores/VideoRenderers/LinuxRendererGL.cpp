da4024c5de7295214097e5a18b68139d2682147c
xbmc <fernetmenta@online.de>
2012-12-12 18:34:47 +0100

    vdpau: drop studio level conversion

--- xbmc/cores/VideoRenderers/LinuxRendererGL.cpp
+++ xbmc/cores/VideoRenderers/LinuxRendererGL.cpp
@@ -3360,7 +3360,7 @@ bool CLinuxRendererGL::Supports(ERENDERFEATURE feature)
 {
   if(feature == RENDERFEATURE_BRIGHTNESS)
   {
-    if ((m_renderMethod & RENDER_VDPAU) && !CSettings::Get().GetBool("videoscreen.limitedrange"))
+    if (m_renderMethod & RENDER_VDPAU)
       return true;
 
     if (m_renderMethod & RENDER_VAAPI)
@@ -3373,7 +3373,7 @@ bool CLinuxRendererGL::Supports(ERENDERFEATURE feature)
   
   if(feature == RENDERFEATURE_CONTRAST)
   {
-    if ((m_renderMethod & RENDER_VDPAU) && !CSettings::Get().GetBool("videoscreen.limitedrange"))
+    if (m_renderMethod & RENDER_VDPAU)
       return true;
 
     if (m_renderMethod & RENDER_VAAPI)
