8699071df2e1d35ed9a1875f2fc1d165f055b4dd
xbmc <fernetmenta@online.de>
2013-03-02 12:10:17 +0100

    renderbuffers: drop enable/disable in this iteration

--- xbmc/cores/VideoRenderers/RenderManager.cpp
+++ xbmc/cores/VideoRenderers/RenderManager.cpp
@@ -274,6 +274,7 @@ bool CXBMCRenderManager::Configure(unsigned int width, unsigned int height, unsi
     m_presentstep = PRESENT_IDLE;
     m_presentevent.Set();
     ResetRenderBuffer();
+    EnableBuffering(buffering);
   }
 
   return result;
@@ -1102,7 +1103,10 @@ void CXBMCRenderManager::EnableBuffering(bool enable)
   CRetakeLock<CExclusiveLock> lock(m_sharedSection);
 
   if (m_iNumRenderBuffers < 3)
+  {
+    m_bUseBuffering = false;
     return;
+  }
 
   m_bUseBuffering = enable;
   if (!m_bUseBuffering)
